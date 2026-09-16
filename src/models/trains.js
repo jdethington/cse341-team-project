3fac5d5315a5d7734fa783d66914d32d375bd766
jdethington <jdethington@gmail.com>
2026-09-16 10:25:00 -0700

    2. Fixed that broken import

--- src/models/trains.js
+++ src/models/trains.js
@@ -1,5 +1,5 @@
 //src/models/trains.js. 
-import Train from "../train.js";
+import Train from "./schemas/train.js";
 
 export async function getTrainById(id) {
   return Train.findOne({ id }).lean();
